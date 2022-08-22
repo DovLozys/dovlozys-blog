ae16709a90cbcf362de992c33608e09f889fb5c2
Dovydas Lozys <755086+DovLozys@users.noreply.github.com>
2022-08-22 22:08:14 +0100

    partial work on blog post page generation

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,38 @@
-exports.createPages = async ({ actions }) => {
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
+  
+  return graphql(`
+  {
+    allMarkdownRemark {
+      edges {
+        node {
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+  `).then((result) => {
+    result.data.allMarkdownRemark.edges.forEach(({node}) => {
+      createPage({
+        path: node.fields.slug,
+        //TODO: add 'component', etc
+      })
+    });
   })
+}
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode })
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slug
+    })
+  }
 }
